e7aa6784c775fa87fce9095dc38c7444f03cacec
ethandmd <ethandmd@protonmail.com>
2024-04-07 00:41:07 -0700

    poc ckpt 4.1
    The roughest possible implementation works ...

--- src/main.rs
+++ src/main.rs
@@ -3,18 +3,17 @@
 #![allow(non_snake_case)]
 #![allow(dead_code)] // For perf bindings
 
+use std::collections::{BinaryHeap, HashMap};
+
 use clap::Parser;
 use log::{debug, error};
 use perf::perf_sys::{
     perf_event_attr, perf_event_header, perf_event_sample_format_PERF_SAMPLE_ADDR,
-    perf_event_sample_format_PERF_SAMPLE_PHYS_ADDR, perf_event_sample_format_PERF_SAMPLE_TID,
-    perf_event_sample_format_PERF_SAMPLE_TIME, perf_type_id_PERF_TYPE_RAW,
+    perf_event_sample_format_PERF_SAMPLE_TID, perf_event_sample_format_PERF_SAMPLE_TIME,
+    perf_type_id_PERF_TYPE_RAW,
 };
 
-use crate::perf::{
-    perf_event_type_PERF_RECORD_EXIT, perf_event_type_PERF_RECORD_LOST,
-    perf_event_type_PERF_RECORD_SAMPLE,
-};
+use crate::perf::{perf_event_sample_format_PERF_SAMPLE_IP, perf_event_type_PERF_RECORD_SAMPLE};
 
 mod perf;
 
@@ -46,45 +45,75 @@ struct Args {
 #[repr(C)]
 struct demo_record {
     hdr: perf_event_header,
+    ip: u64,
     tid: u64,
     time: u64,
     addr: u64,
-    cpu: u32,
-    phys_addr: u64,
 }
 
+#[derive(Copy, Clone, Debug, PartialEq)]
+struct AccessTracker {
+    ewma: f64,
+    page: u64,
+}
+
+const ALPHA: f64 = 0.2;
+
+impl AccessTracker {
+    fn new(page: u64) -> Self {
+        Self { ewma: 0.0, page }
+    }
+
+    fn update(&mut self, new_value: f64) {
+        //self.ewma = (1.0 - ALPHA) * self.ewma + ALPHA * new_value;
+        self.ewma += new_value;
+    }
+}
+
+impl Ord for AccessTracker {
+    fn cmp(&self, other: &Self) -> std::cmp::Ordering {
+        self.ewma.partial_cmp(&other.ewma).unwrap()
+    }
+}
+
+impl PartialOrd for AccessTracker {
+    fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
+        Some(self.cmp(other))
+    }
+}
+
+impl Eq for AccessTracker {}
+
 unsafe fn read_samples(mmap: *mut perf::perf_event_mmap_page) {
+    let mut stat = HashMap::new();
+    let mut heap = BinaryHeap::new();
     println!("tid,time,addr,cpu,phys_addr");
-    let mut lost = 0;
-    let data_begin = mmap.byte_add((*mmap).offset as usize);
-    debug!("Data offset: {:#02x}", (*mmap).offset);
+    let data_begin = mmap.byte_add((*mmap).data_offset as usize);
+    debug!("Data offset: {:#02x}", (*mmap).data_offset);
     debug!("Data size: {:#02x}", (*mmap).data_size);
     debug!("Data begin: {:p}", data_begin);
+    debug!("Region begin: {:p}", mmap);
+    let mut flag = false;
     loop {
         let next = ((*mmap).data_tail % (*mmap).data_size) as usize;
         let hdr: *const perf_event_header = data_begin.byte_add(next).cast();
-        debug!("Header type: {}", (*hdr).type_);
         match (*hdr).type_ {
             perf_event_type_PERF_RECORD_SAMPLE => {
-                // 9
                 let sample: *const demo_record = hdr.cast();
-                if (*sample).addr != 0 {
-                    println!(
-                        "{},{},{},{},{}",
-                        (*sample).tid,
-                        (*sample).time,
-                        (*sample).addr,
-                        (*sample).cpu,
-                        (*sample).phys_addr
-                    );
+                if (*sample).addr == 0 {
+                    continue;
                 }
-            }
-            perf_event_type_PERF_RECORD_EXIT => {
-                debug!("Exit record");
-            }
-            perf_event_type_PERF_RECORD_LOST => {
-                lost += 1;
-                debug!("Lost samples: {}", lost);
+                let page = (*sample).addr & !4095;
+                let record = stat.entry(page).or_insert_with(|| {
+                    flag = true;
+                    debug!("Sample: {:#02x}", page);
+                    AccessTracker::new(page)
+                });
+                (*record).update(1.0);
+                if flag {
+                    heap.push(*record);
+                    flag = false;
+                }
             }
             _ => {
                 //debug!("Unknown record type: {}", (*hdr).type_);
@@ -123,10 +152,12 @@ fn main() {
         panic!();
     };
     attr.sample_period(SAMPLE_PERIOD);
-    attr.sample_type = perf_event_sample_format_PERF_SAMPLE_TID
+    attr.sample_type = perf_event_sample_format_PERF_SAMPLE_IP
+        | perf_event_sample_format_PERF_SAMPLE_TID
         | perf_event_sample_format_PERF_SAMPLE_TIME
-        | perf_event_sample_format_PERF_SAMPLE_ADDR
-        | perf_event_sample_format_PERF_SAMPLE_PHYS_ADDR;
+        | perf_event_sample_format_PERF_SAMPLE_ADDR;
+    //attr.__bindgen_anon_2.wakeup_events = (SAMPLE_PERIOD / 2) as u32;
+    //attr.set_watermark(1);
     attr.set_exclude_hv(1);
     attr.set_exclude_callchain_user(1);
     attr.set_exclude_callchain_kernel(1);
